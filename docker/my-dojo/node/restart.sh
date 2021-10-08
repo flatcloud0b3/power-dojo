3b17a0dda057adfc4b1f70e700bd06d7c146b5ab
Pavel Ševčík <pajaseviwow@gmail.com>
2021-10-08 16:26:07 +0200

    Throw error on unhandled promise rejection

--- docker/my-dojo/node/restart.sh
+++ docker/my-dojo/node/restart.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 cd /home/node/app/accounts
-forever start -a -l /dev/stdout -o /dev/null -e /dev/null index.js
+forever start -c 'node --unhandled-rejections=strict' -a -l /dev/stdout -o /dev/null -e /dev/null index.js
 
 cd /home/node/app/pushtx
-forever start -a -l /dev/stdout -o /dev/null -e /dev/null index.js
-forever start -a -l /dev/stdout -o /dev/null -e /dev/null index-orchestrator.js
+forever start -c 'node --unhandled-rejections=strict' -a -l /dev/stdout -o /dev/null -e /dev/null index.js
+forever start -c 'node --unhandled-rejections=strict' -a -l /dev/stdout -o /dev/null -e /dev/null index-orchestrator.js
 
 cd /home/node/app/tracker
-forever start -a -l /dev/stdout -o /dev/null -e /dev/null index.js
+forever start -c 'node --unhandled-rejections=strict' -a -l /dev/stdout -o /dev/null -e /dev/null index.js
 
 # Keep the container up
 while true
